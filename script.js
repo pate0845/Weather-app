e11f854e91896b570da4f28d77a785f94284278d
pate0845 <pate0845@algonquinlive.com>
2022-02-06 16:35:56 -0500

    Done!

---
+++ script.js
@@ -0,0 +1,48 @@
+let weather={
+    key:'',
+
+    fetchWeather:function(city){
+        fetch(
+            "https://api.openweathermap.org/data/2.5/weather?q="
+             +city+
+             "&units=metric&appid="
+             +this.key
+            )
+            .then((resp)=>resp.json())
+            .then((data)=>this.displayWeather(data));
+    },
+
+    displayWeather:function(data){
+        const {name}=data;
+        const {icon,description}=data.weather[0];
+        const {temp,humidity}=data.main;
+        const {speed}=data.wind;
+        document.querySelector('.city').innerText="Weather in "+name;
+        document.querySelector('.icon').src='https://openweathermap.org/img/wn/'+icon+'@2x.png';
+        document.querySelector('.description').innerText=description;
+        document.querySelector('.humidity').innerText='Humidity: '+humidity+'%';
+        document.querySelector('.temp').innerText=Math.round(temp)+"℃";
+        document.querySelector('.wind').innerText='Wind speed: '+speed+'Km/h';
+        document.querySelector('.weather').classList.remove('loading');
+        document.body.style.backgroundImage="url('https://source.unsplash.com/1600x900?"+name+"')";
+    },
+    search:function(){
+        const input=document.querySelector('.search-bar').value;
+        this.fetchWeather(input);
+    }
+}
+
+
+document.querySelector('.searchButton')
+.addEventListener('click',function(){
+weather.search();
+})
+
+document.querySelector('.search-bar')
+.addEventListener('keyup',function(event){
+    if(event.key=='Enter'){
+        weather.search();
+    }
+})
+
+weather.fetchWeather('Toronto');
